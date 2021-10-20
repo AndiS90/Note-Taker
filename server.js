cfe64edc0e65aa2415c0ba303d7f54005adff849
andi <andrea.strickland1990@gmail.com>
2021-10-20 15:35:34 -0500

    installed nanoid npm, fixed wildcard routing

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { clog } = require('./middleware/clog');
+const {
+  clog
+} = require('./middleware/clog');
 
 //const notes = require('express').Router();
-const notesFile= require('./db/db.json');
+const notesFile = require('./db/db.json');
 const {
   readFromFile,
   readAndAppend,
   writeToFile,
 } = require('./helpers/fsUtils');
+
+const { nanoid } = require('nanoid')
 
 const app = express();
 const PORT = process.env.port || 3001;
@@ -21,62 +25,100 @@ app.use(clog);
 app.use(express.json());
 
 // Middleware for parsing urlencoded form data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
- //serves static files such as images, CSS files, and JavaScript files
+//serves static files such as images, CSS files, and JavaScript files
 app.use(express.static('public'));
 
-//GET * should return the index.html file.
-app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/assets/index.html'))
+//GET "root" (/) should return the index.html file.
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 //GET /notes should return the notes.html file.
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/assets/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-
+//GET wildcard route to direct users back to opening page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
 // res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api/notes', (req, res) => 
-  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
-);
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/notes.json')
+    .then((data) => res.json(JSON.parse(data)))
+});
 
-// GET Route for a specific note
+// GET Route for a specific note 
+// eventally to pull to front, select out of list, maybe delete?
 app.get('notes/:note_id', (req, res) => {
   const noteId = req.params.note_id;
   readFromFile('./db/notes.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       const result = json.filter((note) => app.note_id === noteId);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('No note with that ID');
+      return result.length > 0 ?
+        res.json(result) :
+        res.json('No note with that ID');
     });
 });
 
-app.post('api/notes', (req, res) =>  { 
-// Destructuring assignment for the items in req.body
-const { title, text} = req.body;
 
-// If all the required properties are present
-if (req.body) {
-  // Variable for the object we will save
-  const newNote = {
-    title,
-    text,
-    tip_id: //something... but this comma stays => // ,    
-  };
+// POST to take submitted notes and add them to the list of notes
+app.post('api/notes', (req, res) => {
+   
+  //log that post req was received
+  console.info(`${req.method} request received to add a note`);
 
-  readAndAppend(newNote, './db/notes.json');
-  res.json(`note added successfully 🚀`);
-} else {
-  res.error('Error in adding note');
-}
-}
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    if (req.body) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: nanoid(), //something... but this comma stays => // ,    
+      };
+
+      readAndAppend(newNote, './db/notes.json');
+
+
+
+      res.json(`note added successfully 🚀`);
+      
+      //return newNote;
+    } else {
+      res.json('Error in adding note');
+    }
+  }
 
 
 );
+
+
+
+// DELETE Route for a specific note
+app.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => app.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 
 
 
